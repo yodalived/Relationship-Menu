0db53370a1abc94038e02126c398471eb5481641
Paul-Vincent Roll <paviro@me.com>
2025-05-02 17:32:05 +0200

    Added GitHub link to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,6 +6,7 @@ export default function Footer() {
       <div className="flex gap-4 justify-center">
         <p><Link href="/privacy-policy" className="hover:underline">Privacy Policy</Link></p>
         <p><Link href="/legal-disclosure" className="hover:underline">Legal Disclosure</Link></p>
+        <p><Link href="https://github.com/paviro/Relationship-Menu" className="hover:underline">Source Code</Link></p>
       </div>
     </footer>
   );
