effb011df358d986ef88f7be77dc1f664ed7a0f3
Paul-Vincent Roll <paviro@me.com>
2025-09-29 10:39:45 +0200

    Disable scaling on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className={`${nunito.variable} antialiased`}>
         <ToastProvider>
           <LayoutWrapper>{children}</LayoutWrapper>
